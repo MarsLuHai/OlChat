0717435bea3eba6b2be4a5e1b19870281037ae20
BEDIDI Fawzi <fawzi.bedidi@epitech.eu>
2016-04-14 18:15:07 +0800

    Added signup page; linked it to sign in

--- app/src/main/java/com/bedidi/fawzi/olchat/MainActivity.java
+++ app/src/main/java/com/bedidi/fawzi/olchat/MainActivity.java
@@ -1,18 +1,43 @@
 package com.bedidi.fawzi.olchat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    public final static String EXTRA_MESSAGE = "com.bedidi.fawzi.olchat.USERNAME";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+        usernameEditText = (EditText) findViewById(R.id.username);
+        passwordEditText = (EditText) findViewById(R.id.password);
 
+    }
+
+    protected void signIn(View view){
+
+    }
+
+    protected void signUp(View view){
+        Intent i=new Intent(MainActivity.this, SignUp.class);
+
+        if (usernameEditText.getText().length() >= 1){
+            i.putExtra(EXTRA_MESSAGE, usernameEditText.getText().toString());
+            Log.i("Main Activity","Username sent to the Signup activity");
+        }
+        startActivity(i);
     }
 }
--- app/src/main/java/com/bedidi/fawzi/olchat/SignUp.java
+++ app/src/main/java/com/bedidi/fawzi/olchat/SignUp.java
@@ -1,15 +1,26 @@
 package com.bedidi.fawzi.olchat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private EditText password2EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_sign_up);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        usernameEditText = (EditText) findViewById(R.id.username);
+        passwordEditText = (EditText) findViewById(R.id.password);
+        password2EditText = (EditText) findViewById(R.id.password2);
+        usernameEditText.setText(message);
     }
 }
