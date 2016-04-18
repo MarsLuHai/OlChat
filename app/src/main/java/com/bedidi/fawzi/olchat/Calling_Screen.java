f3c37fc95c8bcc8e034e2312ab76f50d6f7b4e67
Alexboost <alexis.auchere@epitech.eu>
2016-04-18 23:54:32 +0800

    Add the first version of the video calling screen activity Update the Calling Screen Add link between Calling Screen, Video Screen and Contact list.

--- app/src/main/java/com/bedidi/fawzi/olchat/Calling_Screen.java
+++ app/src/main/java/com/bedidi/fawzi/olchat/Calling_Screen.java
@@ -1,7 +1,11 @@
 package com.bedidi.fawzi.olchat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telecom.Call;
+import android.view.View;
+import android.widget.Button;
 
 public class Calling_Screen extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class Calling_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calling_screen);
+    }
+
+    public void hangUp(View view)
+    {
+        Intent ContactPage = new Intent(getApplicationContext(), Contact_page.class);
+        startActivity(ContactPage);
+    }
+
+    public void videoCall(View view)
+    {
+        Intent CallingVideoScreen = new Intent(getApplicationContext(), Calling_Video_Screen.class);
+        startActivity(CallingVideoScreen);
     }
 }
