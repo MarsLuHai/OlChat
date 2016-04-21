62840b201a95a3f02ac4209a80ee5c3cd0831dd2
Baptiste <baptistelefeuvre@hotmail.com>
2016-04-21 17:43:20 +0800

    Ajout fonction zoom avec seekbar dans page de settings et cleaning des layouts de la partie supérieure de la page de contact. Si ça ne fonctionne pas, c'est la faute à Ludovic. <3

---
+++ app/src/main/java/com/bedidi/fawzi/olchat/Settings.java
@@ -0,0 +1,38 @@
+package com.bedidi.fawzi.olchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class Settings extends AppCompatActivity {
+    public SeekBar Font_size;
+    public TextView seekBarValue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+         Font_size = (SeekBar)findViewById(R.id.seekBar2);
+        seekBarValue = (TextView)findViewById(R.id.fontextbox);
+
+        Font_size.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarValue.setTextSize(Font_size.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+
+}
